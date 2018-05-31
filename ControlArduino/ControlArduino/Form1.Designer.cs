e4f853b15b9970fda8509f64bf45877b5753e8e8
GerardoA <luceroriosg@gmail.com>
2018-05-31 13:01:02 -0600

    comentario

---
+++ ControlArduino/ControlArduino/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ControlArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Estado = new System.Windows.Forms.ToolStripStatusLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(607, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ON/OFF";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(610, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Impresora 3D";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Camara web";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(586, 344);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Estado});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(722, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Estado
+            // 
+            this.Estado.Name = "Estado";
+            this.Estado.Size = new System.Drawing.Size(42, 17);
+            this.Estado.Text = "Estado";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(607, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(103, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(610, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seleccionar Camara";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 442);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Monitoreo Impresora 3D";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripStatusLabel Estado;
+    }
+}
+
---
+++ ControlArduino/ControlArduino/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports; // No olvidar.
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace ControlArduino
+{
+    
+    public partial class Form1 : Form
+    {
+        bool arduino = false;
+        private bool ExisteDispositivo = false;
+        private FilterInfoCollection DispositivoDeVideo;
+        private VideoCaptureDevice FuenteDeVideo = null;
+
+        public void CargarDispositivos(FilterInfoCollection Dispositivos)
+        {
+            for (int i = 0; i < Dispositivos.Count; i++) ;
+
+            comboBox1.Items.Add(Dispositivos[0].Name.ToString());
+            comboBox1.Text = comboBox1.Items[0].ToString();
+
+        }
+
+        public void BuscarDispositivos()
+        {
+            DispositivoDeVideo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (DispositivoDeVideo.Count == 0)
+            {
+                ExisteDispositivo = false;
+            }
+
+            else
+            {
+                ExisteDispositivo = true;
+                CargarDispositivos(DispositivoDeVideo);
+
+            }
+        }
+
+        public void TerminarFuenteDeVideo()
+        {
+            if (!(FuenteDeVideo == null))
+                if (FuenteDeVideo.IsRunning)
+                {
+                    FuenteDeVideo.SignalToStop();
+                    FuenteDeVideo = null;
+                }
+
+        }
+        public  void Video_NuevoFrame( object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = Imagen;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            BuscarDispositivos();
+            // Abrir puerto mientras se ejecuta esta aplicación.
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Open();
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arduino = !arduino;
+            Color background;
+            byte[] mBuffer;
+            if (arduino){
+                mBuffer = Encoding.ASCII.GetBytes("Led_ON");
+                background = Color.Green;
+            }
+            else{
+                mBuffer = Encoding.ASCII.GetBytes("Led_OFF");
+                background = Color.Red;
+            }
+
+            button1.BackColor = background;
+            serialPort1.Write(mBuffer, 0, mBuffer.Length);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ExisteDispositivo){
+                FuenteDeVideo = new VideoCaptureDevice(DispositivoDeVideo[comboBox1.SelectedIndex].MonikerString);
+                FuenteDeVideo.NewFrame += new NewFrameEventHandler(Video_NuevoFrame);
+                FuenteDeVideo.Start();
+                Estado.Text = "Ejecutando Dispositivo…";
+                comboBox1.Enabled = false;
+            }
+            else
+                Estado.Text = "Error: No se encuenta el Dispositivo";
+
+            if (FuenteDeVideo.IsRunning){
+                TerminarFuenteDeVideo();
+                Estado.Text = "Dispositivo Detenido…";
+                comboBox1.Enabled = true;
+
+            }
+        }
+    }
+}
